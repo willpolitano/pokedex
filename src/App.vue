b4c16f521ed6e0561e029787c70b358a5a02418b
willpolitano <willianpolitano@gmail.com>
2020-12-03 09:54:46 -0300

    consumindo api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(pokemon,index) in pokemons" :key="index">
+      <Pokemon :name="pokemon.name" :url="pokemon" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?offset=151&limit=151").then(res => {
+      this.pokemons = res.data.results
+    })
   }
 }
 </script>
